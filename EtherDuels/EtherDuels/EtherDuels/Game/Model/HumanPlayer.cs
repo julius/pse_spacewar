98e97d3fc282957edbb185737b7943a5459038d3
unknown <s_jecker@.atis-stud.uni-karlsruhe.de>
2010-12-15 17:44:37 +0100

    Started to work on Player Classes

---
+++ EtherDuels/EtherDuels/EtherDuels/Game/Model/HumanPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace EtherDuels.Game.Model
+{
+    class HumanPlayer: Player
+    {
+        // TODO Constructor
+
+        public void Update(GameTime gameTime)
+        {
+            // TODO
+        }
+    }
+}
---
+++ EtherDuels/EtherDuels/EtherDuels/Game/Model/Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace EtherDuels.Game.Model
+{
+    abstract class Player
+    {
+        // TODO Constructor
+
+        public int GetPlayerId()
+        {
+            // TODO
+            return 0;
+        }
+
+        public int GetPoints()
+        {
+            // TODO
+            return 0;
+        }
+
+        public void SetPoints(int points)
+        {
+            // TODO
+        }
+
+        public Spaceship GetSpaceship()
+        {
+            // TODO
+            return null;
+        }
+
+        public void SetSpaceship(Spaceship spaceship)
+        {
+            // TODO
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            // TODO
+        }
+    }
+}
