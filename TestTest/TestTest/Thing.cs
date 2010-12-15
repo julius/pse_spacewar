c34e912731792ee4f758ac6a87e73d079e1da7ae
Julius Neuffer <julius.neuffer@gmail.com>
2010-12-15 18:06:34 +0100

    Changed Test example

---
+++ TestTest/TestTest/Thing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestTest
+{
+    class Thing
+    {
+        private int property;
+
+        public int Property
+        {
+            get { return property; }
+            set { property = value; }
+        }
+    }
+}
