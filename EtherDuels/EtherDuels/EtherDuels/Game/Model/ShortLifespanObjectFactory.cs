aafa8449c0e4bad46aa4ad2759e58d6c5a8c9a57
Claudilein <claudilein@gmail.com>
2010-12-17 16:28:31 +0100

    ShortLifespanObjectFactory angefangen

---
+++ EtherDuels/EtherDuels/EtherDuels/Game/Model/ShortLifespanObjectFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EtherDuels.Game.Model
+{
+    interface ShortLifespanObjectFactory
+    {
+        public Explosion CreateExplosion();
+        public Projectile CreateProjectile();
+        public ExplosionView CreateExplosionView(Explosion explosion);
+        public ProjectileView CreateProjectileview(Projectile projectile);
+    }
+}
