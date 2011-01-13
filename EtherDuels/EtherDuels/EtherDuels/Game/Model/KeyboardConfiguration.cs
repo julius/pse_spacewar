3b31e143e678cdb328ca507e614e3673a6a0bac1
Julius Neuffer <julius.neuffer@gmail.com>
2011-01-13 14:21:55 +0100

    KeyboardConfiguration and InputConfigurationRetriever now in the right place

--- EtherDuels/EtherDuels/EtherDuels/Game/Model/KeyboardConfiguration.cs
+++ EtherDuels/EtherDuels/EtherDuels/Game/Model/KeyboardConfiguration.cs
@@ -3,62 +3,124 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework.Input;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
 
 namespace EtherDuels.Game.Model
-{   
+{
+    //KeyboardConfiguration has to be serializable as well as Configuration, 
+    // if Configuration is supposed to store KeyboardConfiguration as well if it is serialized
     /// <summary>
     /// 
     /// </summary>
+    [Serializable()]
     class KeyboardConfiguration : InputConfigurationRetriever
     {
-        private Keys backwardKey;
-        private Keys forwardKey;
-        private Keys leftKey;
-        private Keys rightKey;
-        private Keys fireKey;
-        private Keys nextWeaponKey;
-        private Keys prevWeaponKey;
-
-        public Keys ForwardKey
+        //Deserialization constructor.
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="ctxt"></param>
+        public KeyboardConfiguration(SerializationInfo info, StreamingContext ctxt)
         {
-            get { return forwardKey; }
-            set { forwardKey = value; }
+            this.backward   = (Keys)info.GetValue("backward", typeof(Keys));
+            this.fire       = (Keys)info.GetValue("fire", typeof(Keys));
+            this.forward    = (Keys)info.GetValue("forward", typeof(Keys));
+            this.left       = (Keys)info.GetValue("left", typeof(Keys));
+            this.nextWeapon = (Keys)info.GetValue("nextWeapon", typeof(Keys));
+            this.prevWeapon = (Keys)info.GetValue("prevWeapon", typeof(Keys));
+            this.right      = (Keys)info.GetValue("right", typeof(Keys));
         }
 
-        public Keys LeftKey
+        //Serialization function.
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="ctxt"></param>
+        public void GetObjectData(SerializationInfo info, StreamingContext ctxt)
         {
-            get { return leftKey; }
-            set { leftKey = value; }
+            info.AddValue("backward", backward);
+            info.AddValue("fire", fire);
+            info.AddValue("forward", forward);
+            info.AddValue("left", left);
+            info.AddValue("nextWeapon", nextWeapon);
+            info.AddValue("prevWeapon", prevWeapon);
+            info.AddValue("right", right);
         }
 
-        public Keys RightKey
+        private Keys backward;
+           public Keys Backward
         {
-            get { return rightKey; }
-            set { rightKey = value; }
+            get { return backward; }
+            set { backward = value; }
         }
 
-        public Keys FireKey
+        private Keys fire;
+        public Keys Fire
         {
-            get { return fireKey; }
-            set { fireKey = value; }
+            get { return fire; }
+            set { fire = value; }
         }
 
-        public Keys NextWeaponKey
+        private Keys forward;
+        public Keys Forward
         {
-            get { return nextWeaponKey; }
-            set { nextWeaponKey = value; }
+            get { return forward; }
+            set { forward = value; }
         }
 
-        public Keys PrevWeaponKey
+        private Keys left;
+        public Keys Left
         {
-            get { return prevWeaponKey; }
-            set { prevWeaponKey = value; }
+            get { return left; }
+            set { left = value; }
+        }
+        
+        private Keys nextWeapon;
+        public Keys NextWeapon
+        {
+            get { return nextWeapon; }
+            set { nextWeapon = value; }
         }
 
-        public Keys BackwardKey
+        private Keys prevWeapon;
+        public Keys PrevWeapon
         {
-            get { return backwardKey; }
-            set { backwardKey = value; }
+            get { return prevWeapon; }
+            set { prevWeapon = value; }
+        }
+
+        private Keys right;
+        public Keys Right
+        {
+            get { return right; }
+            set { right = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="secKeyConf"></param>
+        /// <returns></returns>
+        public bool Equals(KeyboardConfiguration secKeyConf)
+        {
+            if (secKeyConf == null)
+                return false;
+
+            // all the same
+            if (this.backward != secKeyConf.backward
+                || this.fire != secKeyConf.fire
+                || this.forward != secKeyConf.forward
+                || this.left != secKeyConf.left
+                || this.nextWeapon != secKeyConf.nextWeapon
+                || this.prevWeapon != secKeyConf.prevWeapon
+                || this.right != secKeyConf.right)
+                return false;
+
+            return true;
         }
     }
 }
