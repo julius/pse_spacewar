1591bf37d6228fd61e8a3b2be9357bc7d790b71c
Philip Ahlers <philip.ahlers@gmail.com>
2011-01-14 14:42:16 +0100

    Config Stuff

---
+++ EtherDuels/EtherDuels/EtherDuels/Game/ConfigurationRetriever.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EtherDuels.Game
+{
+    interface ConfigurationRetriever
+    {
+        int GetDifficulty();
+        int GetDifficultyAI();
+        KeyboardConfiguration GetKeyboardConfiguration(int playerID);
+    }
+}
---
+++ EtherDuels/EtherDuels/EtherDuels/Game/InputConfigurationRetriever.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace EtherDuels.Game
+{
+    interface InputConfigurationRetriever
+    {
+        /*
+        Keys GetBackwardKey();
+        Keys GetFireKey();
+        Keys GetForwardKey();
+        Keys GetLeftKey();
+        Keys GetNextWeaponKey();
+        Keys GetPrevWeaponKey();
+        Keys GetRightKey();
+         */
+    }
+}
